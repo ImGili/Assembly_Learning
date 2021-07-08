3e8219a29b4b5d3eb5e8b63365b8d472f551e2e1
xhguleixin123 <18017158729@163.com>
2021-07-08 23:19:14 +0800

    REDME.md

--- codes/test.asm
+++ codes/test.asm
@@ -1,5 +1,12 @@
-assume cs:codesg
-
-codesg segment 
-
-    move
+        global  main
+        extern  puts
+        section .text
+main:
+        sub     rsp, 28h                        
+        mov     rcx, message
+        mov     ax, bx
+        call    puts                           
+        add     rsp, 28h                        
+        ret
+message:
+        db      'Hello', 0                     
